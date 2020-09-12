6c43c33eae4377de9c37f600e60988db603103ad
kazunari0220 <67147883+kazunari0220@users.noreply.github.com>
2020-09-12 17:54:35 +0900

    profile

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,10 @@
 class PagesController < ApplicationController
   def index
   end
+
+  def profile
+  end
+
+  def works
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'pages', to: 'pages#index'
-
+  get 'profile', to: 'pages#profile'
+  get 'works', to: 'pages#works'
   root 'pages#index'
 end

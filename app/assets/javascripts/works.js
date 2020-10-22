1326cf40f2f782803943ed36681ad0f54a38953a
kazunari0220 <67147883+kazunari0220@users.noreply.github.com>
2020-10-22 22:17:37 +0900

    laravelblog

--- app/assets/javascripts/works.js
+++ app/assets/javascripts/works.js
@@ -66,3 +66,20 @@ $(function(){
   $next.fadeIn($fade_speed).addClass("active");
   },$interval);
 });
+
+$(function(){
+  // 設定
+  var $width =300; // 横幅
+  var $height =200; // 高さ
+  var $interval = 1200; // 切り替わりの間隔（ミリ秒）
+  var $fade_speed = 1000; // フェード処理の早さ（ミリ秒）
+  $("#slide5 ul li").css({"overflow":"hidden","width":$width,"height":$height});
+  $("#slide5 ul li").hide().css({"top":0,"left":0});
+  $("#slide5 ul li:first").addClass("active").show();
+  setInterval(function(){
+  var $active = $("#slide5 ul li.active");
+  var $next = $active.next("li").length?$active.next("li"):$("#slide5 ul li:first");
+  $active.fadeOut($fade_speed).removeClass("active");
+  $next.fadeIn($fade_speed).addClass("active");
+  },$interval);
+});
